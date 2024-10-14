d92999bc6218df03694e6b66b73ea4b03e0ec12a
KaytoKidd <rpp2022-7135-98184@bicol-u.edu.ph>
2024-10-14 18:50:22 +0800

    SRC for UserInput to Array.

---
+++ User Interaction Parts/userInput_toArray.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void get_user_input();
+void get_user_input() {
+
+    char table_guide[] = {'a', 'b', 'c', 'd', 
+                          'e', 'f', 'g', 'h', 
+                          'i', 'j', 'k', 'l', 
+                          'm', 'n', 'o', 'p'};
+    char table_input[16];
+
+    int i = 0;
+    char user_input = ' ';
+
+    for(i = 0; i < 16; i++) {
+
+        printf("%c = ", table_guide[i]);
+        scanf(" %c", &user_input);
+
+        if (user_input == 'B')
+            table_input[i] = ' ';
+        else
+            table_input[i] = user_input;
+            
+    }
+
+    // Printing the array.
+    printf("\n\n");
+    for(i = 0; i < 16; i++) {
+        printf("%c\n", table_input[i]);
+    }
+
+}
+
+void main() {
+
+    get_user_input();
+
+}
