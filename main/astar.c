1dbe9d3cb5dd1ba6c9dd8540c39a7698cfc1a738
KaytoKidd <rpp2022-7135-98184@bicol-u.edu.ph>
2024-10-20 17:47:22 +0800

    Main SRC for A Star Search Algorithm.

---
+++ main/astar.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ids_LL {
+
+    char data;
+    struct ids_LL *next;
+
+};
+struct astar_LL {
+
+    char data;
+    struct astar_LL *next;
+
+};
+
+struct ids_LL *ids_LL_HEAD = NULL;
+struct astar_LL *astar_LL_HEAD = NULL;
+
+void fixed_user_input();
+void pass_to_ids_LL(int which_table[], int length);
+void pass_to_astar_LL(int which_table[], int length);
+void print_ids_LL();
+void delete_ids_LL();
+
+void fixed_user_input() {
+
+    int easy_table[] = {
+                            4, 2, 3, -1,
+                            5, 1, 6, 7,
+                            8, 9, 10, 11,
+                            12, 13, 14, 15
+                       };
+    int medium_table[] = {
+                            5, 6, 2, 3,
+                            1, -1, 10, 7,
+                            4, 13, 9, 15,
+                            8, 12, 11, 14
+                          };
+    int hard_table[] = {
+                            1, 2, 3, 4,
+                            5, 6, 7, 8,
+                            9, 10, 11, 15,
+                            13, 14, 12, -1
+                       };
+    int worst_table[] = {
+                            1, 6, 2, 3,
+                            5, 10, 7, 4,
+                            9, -1, 11, 8,
+                            13, 14, 15, 12
+                        };
+
+    pass_to_ids_LL(easy_table, 16);
+    pass_to_ids_LL(medium_table, 16);
+    pass_to_ids_LL(hard_table, 16);
+    pass_to_ids_LL(worst_table, 16);
+
+}
+
+void pass_to_ids_LL(int which_table[], int length) {
+
+    int i = 0;
+    for(i = 0; i < length; i++) {
+
+        struct ids_LL *new_node = malloc(sizeof(struct ids_LL *));
+        struct ids_LL *iterator = ids_LL_HEAD;
+        
+        if(ids_LL_HEAD == NULL) {
+            ids_LL_HEAD = new_node;
+        } else {
+            while(iterator -> next != NULL) {
+                iterator = iterator -> next;
+            }
+            iterator -> next = new_node;
+        }
+
+        new_node -> data = which_table[i];
+        new_node -> next = NULL;
+
+    }
+    
+    print_ids_LL();
+
+}
+
+// Temporary, just to see that array is inserted successfully in linkedlist.
+// Including the counter for 4 digits per row.
+// Used \t to make spaces equal, forming a 4x4 square pattern.
+void print_ids_LL() {
+
+    struct ids_LL *printer = ids_LL_HEAD;
+    int counter = 1;
+
+    while(printer != NULL) {
+
+        if(printer -> data == -1)
+            printf(" \t");
+        else
+            printf("%d\t", printer -> data);
+
+        printer = printer -> next;
+        if(counter % 4 == 0)
+            printf("\n");
+        counter += 1;
+    }
+
+    printf("\n");
+    delete_ids_LL();
+
+}
+
+void delete_ids_LL() {
+
+    struct ids_LL *deleter = ids_LL_HEAD;
+    struct ids_LL *iterator = ids_LL_HEAD;
+
+    while(iterator != NULL) {
+        deleter = iterator;
+        iterator = iterator -> next;
+        free(deleter);
+    }
+
+    ids_LL_HEAD = NULL;
+
+}
+
+void main() {
+
+    fixed_user_input();
+
+}
