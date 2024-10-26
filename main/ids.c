683c89c71c3ef7694818a209822a70e53331caa3
KaytoKidd <rpp2022-7135-98184@bicol-u.edu.ph>
2024-10-27 02:26:55 +0800

    Added hierarchy for movements, 3D array.

--- main/ids.c
+++ main/ids.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// board_guide(), get_user_input(), and print_user_input() are all for user interaction only.
+// However, user_input[] will be used for the entire program.
+
+// User Interaction Functions - Declaration
 void board_guide();
 void get_user_input(char board_guide_label[]);
 void print_user_input(int user_input[]);
+
+// Calculation Functions - Declaration
+void heirarchy_of_movements();
 
 void board_guide() {
 
@@ -95,49 +102,35 @@ void print_user_input(int user_input[]) {
 
 }
 
+void heirarchy_of_movements() {
+
+    // Hierarchy of Priority: UP -> DOWN -> LEFT -> RIGHT
+    // UP       - 96
+    // DOWN     - 97
+    // LEFT     - 98
+    // RIGHT    - 99
+
+    int movements[16][3][4] = {
+        {{1, 0, 0, 0}, {5, 2, 0, 0}, {96, 99, 0, 0}},
+        {{2, 0, 0, 0}, {6, 1, 3, 0}, {97, 98, 99, 0}},
+        {{3, 0, 0, 0}, {7, 2, 4, 0}, {97, 98, 99, 0}},
+        {{4, 0, 0, 0}, {8, 3, 0, 0}, {97, 98, 0, 0}},
+        {{5, 0, 0, 0}, {1, 9, 6, 0}, {96, 97, 99, 0}},
+        {{6, 0, 0, 0}, {2, 10, 5, 7}, {96, 97, 98, 99}},
+        {{7, 0, 0, 0}, {3, 11, 6, 8}, {96, 97, 98, 99}},
+        {{8, 0, 0, 0}, {4, 12, 7, 0}, {96, 97, 98, 0}},
+        {{9, 0, 0, 0}, {5, 13, 10, 0}, {96, 97, 99, 0}},
+        {{10, 0, 0, 0}, {6, 14, 9, 11}, {96, 97, 98, 99}},
+        {{11, 0, 0, 0}, {7, 15, 10, 12}, {96, 97, 98, 99}},
+        {{12, 0, 0, 0}, {8, 16, 11, 0}, {96, 97, 98, 0}},
+        {{13, 0, 0, 0}, {9, 14, 0, 0}, {96, 99, 0, 0}},
+        {{14, 0, 0, 0}, {10, 13, 15, 0}, {96, 98, 99, 0}},
+        {{15, 0, 0, 0}, {11, 14, 16, 0}, {96, 98, 99, 0}},
+        {{16, 0, 0, 0}, {12, 15, 0, 0}, {96, 98, 0, 0}}
+    };
+
+}
+
 void main() {
     board_guide();
 }
-
-/*
-
-temporary
-printf("\t+---------------+---------------+---------------+---------------+\n");
-    printf("\t|\t\t|\t\t|\t\t|\t\t|\n"); printf("\t|\t\t|\t\t|\t\t|\t\t|\n");
-    printf("\t|\ta\t|\tb\t|\tc\t|\td\t|\n");
-    printf("\t|\t\t|\t\t|\t\t|\t\t|\n"); printf("\t|\t\t|\t\t|\t\t|\t\t|\n");
-    printf("\t+---------------+---------------+---------------+---------------+\n");
-    printf("\t|\t\t|\t\t|\t\t|\t\t|\n"); printf("\t|\t\t|\t\t|\t\t|\t\t|\n");
-    printf("\t|\te\t|\tf\t|\tg\t|\th\t|\n");
-    printf("\t|\t\t|\t\t|\t\t|\t\t|\n"); printf("\t|\t\t|\t\t|\t\t|\t\t|\n");
-    printf("\t+---------------+---------------+---------------+---------------+\n");
-    printf("\t|\t\t|\t\t|\t\t|\t\t|\n"); printf("\t|\t\t|\t\t|\t\t|\t\t|\n");
-    printf("\t|\ti\t|\tj\t|\tk\t|\tl\t|\n");
-    printf("\t|\t\t|\t\t|\t\t|\t\t|\n"); printf("\t|\t\t|\t\t|\t\t|\t\t|\n");
-    printf("\t+---------------+---------------+---------------+---------------+\n");
-    printf("\t|\t\t|\t\t|\t\t|\t\t|\n"); printf("\t|\t\t|\t\t|\t\t|\t\t|\n");
-    printf("\t|\tm\t|\tn\t|\to\t|\tp\t|\n");
-    printf("\t|\t\t|\t\t|\t\t|\t\t|\n"); printf("\t|\t\t|\t\t|\t\t|\t\t|\n");
-    printf("\t+---------------+---------------+---------------+---------------+\n");
-
-printf("\t+-------+-------+-------+-------+\n");
-    printf("\t|       |       |       |       |\n");
-    for (i = 0; i <= 3; i++) {
-        if (user_input[i] >= 10) {printf("   %d  |", user_input[i]);} else {printf("   %d   |", user_input[i]);}
-    }
-    printf("\t|   %d   |   %d   |   %d   |   %d   |\n", user_input[0], user_input[1], user_input[2], user_input[3]);
-    printf("\t|       |       |       |       |\n");
-    printf("\t+-------+-------+-------+-------+\n");
-    printf("\t|       |       |       |       |\n");
-    printf("\t|   %d   |   %d   |   %d   |   %d   |\n", user_input[4], user_input[5], user_input[6], user_input[7]);
-    printf("\t|       |       |       |       |\n");
-    printf("\t+-------+-------+-------+-------+\n");
-    printf("\t|       |       |       |       |\n");
-    printf("\t|   %d   |   %d   |   %d   |   %d   |\n", user_input[8], user_input[9], user_input[10], user_input[11]);
-    printf("\t|       |       |       |       |\n");
-    printf("\t+-------+-------+-------+-------+\n");
-    printf("\t|       |       |       |       |\n");
-    printf("\t|   %d   |   %d   |   %d   |   %d   |\n", user_input[12], user_input[13], user_input[14], user_input[15]);
-    printf("\t|       |       |       |       |\n");
-    printf("\t+-------+-------+-------+-------+\n");
-*/
