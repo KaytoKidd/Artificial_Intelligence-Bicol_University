51e79904a432270772589590465a9f1f0c953be5
KaytoKidd <rpp2022-7135-98184@bicol-u.edu.ph>
2024-10-29 13:26:14 +0800

    Added code for USER INPUT to FRINGE_LL.

--- main/ids.c
+++ main/ids.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct ExpandedLL {
+    int expanded_arr[16];
+    struct ExpandedLL *next;
+};
+
+struct FringeLL {
+    int level;
+    int fringe_arr[16];
+    struct FringeLL *next;
+};
+
+struct ExpandedLL *expandedLL_head = NULL;
+struct FringeLL *fringeLL_head = NULL;
+
 // board_guide(), get_user_input(), and print_user_input() are all for user interaction only.
 // However, user_input[] will be used for the entire program.
 
@@ -11,6 +25,10 @@ void print_user_input(int user_input[]);
 
 // Calculation Functions - Declaration
 void heirarchy_of_movements();
+
+// Inserting Array to LinkedList
+void insert_to_FringeLL(int the_level, int the_arr[16]);
+void print_FringeLL(); // Debugging purposes, DELETE LATER.
 
 void board_guide() {
 
@@ -64,6 +82,7 @@ void get_user_input(char board_guide_label[]) {
     }
 
     print_user_input(user_input);
+    insert_to_FringeLL(0, user_input); // First/Initial value of Fringe. Level = 0 (ROOT)
 
 }
 
@@ -136,6 +155,49 @@ void heirarchy_of_movements() {
         {{15}, {11, 14, 16}, {96, 98, 99}},
         {{16}, {12, 15}, {96, 98}}
     };
+
+}
+
+// COMPUTATION START - IDS
+void insert_to_FringeLL(int the_level, int the_arr[16]) {
+
+    struct FringeLL *new_node = (struct FringeLL *)malloc(sizeof(struct FringeLL));
+    struct FringeLL *iterator = fringeLL_head;
+    int i = 0;
+
+    if (fringeLL_head == NULL) {
+        fringeLL_head = new_node;
+    } else {
+        iterator = fringeLL_head;
+        while (iterator -> next != NULL) {
+            iterator = iterator -> next;
+        }
+        iterator -> next = new_node;
+    }
+
+    new_node -> level = the_level;
+    for (i = 0; i < 16; i++) {
+        new_node -> fringe_arr[i] = the_arr[i];
+    }
+    new_node -> next = NULL;
+
+    print_FringeLL(); // Debugging purposes, DELETE LATER.
+
+}
+
+void print_FringeLL() { // Debugging purposes, DELETE LATER.
+
+    struct FringeLL *printer = fringeLL_head;
+    int i = 0;
+
+    while (printer != NULL) {
+        printf("Level [%d] = ", printer -> level);
+        for (i = 0; i < 16; i++) {
+            printf("%d ", printer -> fringe_arr[i]);
+        }
+        printf("\n");
+        printer = printer -> next;
+    }
 
 }
 
