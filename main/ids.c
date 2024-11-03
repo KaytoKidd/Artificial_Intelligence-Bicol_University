85f0b855c6b0ee1879e1710264c3ce982792f8d5
KaytoKidd <rpp2022-7135-98184@bicol-u.edu.ph>
2024-11-03 17:49:41 +0800

    Check if goal state good. Create algo for solution path insertion.

--- main/ids.c
+++ main/ids.c
@@ -43,6 +43,13 @@ struct SolutionPathLL *solutionPath_arr[4];    // Where the solution path will b
         - If ULTIMATE PARENT (LEVEL 0) is expanded and found CHILDREN is only 2-3, then empty node will have value of 0.
 */
 int what_level = 0;
+
+int goal_state[16] = {
+    -1, 1, 2, 3,
+    4, 5, 6, 7,
+    8, 9, 10, 11,
+    12, 13, 14, 15
+};
 // GLOBAL DECLARATIONS (Array, Variables, etc.) [END]
 
 // board_guide(), get_user_input(), and print_user_input() are all for user interaction only.
@@ -61,6 +68,7 @@ void expand_this(int deepestNode_index);
 void start_expanding(int the_arr[16], int the_movements[4], int the_movements_label[4], int index_of_negativeOne);
 int check_ifAlready_inExpandedLL(int the_arr[16]);
 int find_deepestNode_inFringe();
+int check_if_goalState(int the_arr[16], int the_goal_state[16]);
 
 void board_guide() {
 
@@ -380,98 +388,103 @@ void start_expanding(int the_arr[16], int the_movements[4], int the_movements_la
 
         }
 
-        // EXPANSION/ADDING - Add to FRINGE LinkedList. [START]
-        int the_movement = 1;
-        int i = 0;
+        if(check_if_goalState(the_arr, goal_state) == 1) { // Check whether already the goal state.
+            // end program.
+            // return solution path and requirements needed by madam.
+        } else {
+            // EXPANSION/ADDING - Add to FRINGE LinkedList. [START]
+            int the_movement = 1;
+            int i = 0;
 
-        // Get first total of non zero values in the_movements[4].
-        int a = 0;
-        int total_of_nonzero = 0;
-        int total_of_nonzero_checker = 0;
+            // Get first total of non zero values in the_movements[4].
+            int a = 0;
+            int total_of_nonzero = 0;
+            int total_of_nonzero_checker = 0;
 
-        for(a = 0; a < 4; a++) {
-            if (the_movements[a] != 0) {
-                total_of_nonzero++;
-            }
-        }
-
-        while(the_movement != 0 || i != 4) {    // i == 3 to stop the loop if movements total is 4.
-
-            /*
-            - Since we already know the index of negative one using index_of_negativeOne, we can use
-            this to exchange with the values of the_movement per iteration 'i' found at the_movements[i].
-
-            LOGIC:
-            - Per iteration, we can exchange the index of index_of_negativeOne with the_movement (which has value of
-            the_movements[i]) and a new array is formed.
-            - Note that the_arr[16] which is passed on the argument should not be changed value as it will be used
-            throughout this function.
-            - TODO:
-                1. Create a new array (new_arr[16]), where this array is passed the value of the_arr[16].
-                2. new_arr[16] will then be the one manipulated (exchange of index of -1 with the_movement).
-                3. Once index is exchanged, pass new_arr[16] to FRINGE using struct malloc.
-                4. LOOP '1.' until the_movement != 0 or i == 3 is true.
-            */
-
-            struct FringeLL *fringe_new_node = (struct FringeLL *)malloc(sizeof(struct FringeLL));
-            struct FringeLL *fringe_new_iterator = fringeLL_head;
-            int new_arr[16];
-            int p = 0;
-            int temp = 0;
-
-            // Pass value of the_arr to new_arr.
-            for(p = 0; p < 16; p++) {
-                new_arr[p] = the_arr[p];
-            }
-
-            // Exchange value.
-            the_movement = the_movements[i];                        // the_movement = 3
-            temp = new_arr[index_of_negativeOne];                   // temp         = new_arr[7]
-            new_arr[index_of_negativeOne] = new_arr[the_movement];  // new_arr[7]   = new_arr[3]
-            new_arr[the_movement] = temp;                           // new_arr[3]   = temp
-            // Now, the index has been exchanged.
-            // new_arr is the array to be inserted to the FRINGE LinkedList.
-
-            if (check_ifAlready_inExpandedLL(new_arr) == 1) {
-
-                // Insert to FRINGE LinkedList.
-                if(fringeLL_head == NULL) {
-                    fringeLL_head = fringe_new_node;
-                } else {
-                    fringe_new_iterator = fringeLL_head;
-                    while(fringe_new_iterator -> next != NULL) {
-                        fringe_new_iterator = fringe_new_iterator -> next;
-                    }
-                    fringe_new_iterator -> next = fringe_new_node;
+            for(a = 0; a < 4; a++) {
+                if (the_movements[a] != 0) {
+                    total_of_nonzero++;
                 }
+            }
 
-                fringe_new_node -> level = what_level;
+            while(the_movement != 0 || i != 4) {    // i == 3 to stop the loop if movements total is 4.
+
+                /*
+                - Since we already know the index of negative one using index_of_negativeOne, we can use
+                this to exchange with the values of the_movement per iteration 'i' found at the_movements[i].
+
+                LOGIC:
+                - Per iteration, we can exchange the index of index_of_negativeOne with the_movement (which has value of
+                the_movements[i]) and a new array is formed.
+                - Note that the_arr[16] which is passed on the argument should not be changed value as it will be used
+                throughout this function.
+                - TODO:
+                    1. Create a new array (new_arr[16]), where this array is passed the value of the_arr[16].
+                    2. new_arr[16] will then be the one manipulated (exchange of index of -1 with the_movement).
+                    3. Once index is exchanged, pass new_arr[16] to FRINGE using struct malloc.
+                    4. LOOP '1.' until the_movement != 0 or i == 3 is true.
+                */
+
+                struct FringeLL *fringe_new_node = (struct FringeLL *)malloc(sizeof(struct FringeLL));
+                struct FringeLL *fringe_new_iterator = fringeLL_head;
+                int new_arr[16];
+                int p = 0;
+                int temp = 0;
+
+                // Pass value of the_arr to new_arr.
                 for(p = 0; p < 16; p++) {
-                    fringe_new_node -> fringe_arr[p] = new_arr[p];
+                    new_arr[p] = the_arr[p];
                 }
-                fringe_new_node -> next = NULL;
 
-            } else {
-                total_of_nonzero_checker++;
-                // This variable is used to count whether all cannot anymore be expanded.
-                // Hence, we can say that the parent does not anymore have a child.
-                // Therefore, we can set what_level to the highest level found within the fringe.
+                // Exchange value.
+                the_movement = the_movements[i];                        // the_movement = 3
+                temp = new_arr[index_of_negativeOne];                   // temp         = new_arr[7]
+                new_arr[index_of_negativeOne] = new_arr[the_movement];  // new_arr[7]   = new_arr[3]
+                new_arr[the_movement] = temp;                           // new_arr[3]   = temp
+                // Now, the index has been exchanged.
+                // new_arr is the array to be inserted to the FRINGE LinkedList.
+
+                if (check_ifAlready_inExpandedLL(new_arr) == 1) {
+
+                    // Insert to FRINGE LinkedList.
+                    if(fringeLL_head == NULL) {
+                        fringeLL_head = fringe_new_node;
+                    } else {
+                        fringe_new_iterator = fringeLL_head;
+                        while(fringe_new_iterator -> next != NULL) {
+                            fringe_new_iterator = fringe_new_iterator -> next;
+                        }
+                        fringe_new_iterator -> next = fringe_new_node;
+                    }
+
+                    fringe_new_node -> level = what_level;
+                    for(p = 0; p < 16; p++) {
+                        fringe_new_node -> fringe_arr[p] = new_arr[p];
+                    }
+                    fringe_new_node -> next = NULL;
+
+                } else {
+                    total_of_nonzero_checker++;
+                    // This variable is used to count whether all cannot anymore be expanded.
+                    // Hence, we can say that the parent does not anymore have a child.
+                    // Therefore, we can set what_level to the highest level found within the fringe.
+                }
+
+                i++;
             }
 
-            i++;
+            if(total_of_nonzero == total_of_nonzero_checker) {
+                // It means that there is no node expanded.
+                // what_level = the next highest level in the FRINGE_LL.
+                what_level = find_deepestNode_inFringe();
+            }
+            // EXPANSION/ADDING - Add to FRINGE LinkedList. [END]
+
+            // ADDING - Add to SOLUTIONPATH LinkedList. [START]
+            struct SolutionPathLL *new_node = (struct SolutionPathLL *)malloc(sizeof(struct SolutionPathLL));
+
+            // ADDING - Add to SOLUTIONPATH LinkedList. [END]
         }
-
-        if(total_of_nonzero == total_of_nonzero_checker) {
-            // It means that there is no node expanded.
-            // what_level = the next highest level in the FRINGE_LL.
-            what_level = find_deepestNode_inFringe();
-        }
-        // EXPANSION/ADDING - Add to FRINGE LinkedList. [END]
-
-        // ADDING - Add to SOLUTIONPATH LinkedList. [START]
-        struct SolutionPathLL *new_node = (struct SolutionPathLL *)malloc(sizeof(struct SolutionPathLL));
-
-        // ADDING - Add to SOLUTIONPATH LinkedList. [END]
 
     } else {
         what_level = find_deepestNode_inFringe();
@@ -523,6 +536,26 @@ int find_deepestNode_inFringe() {
     return deepest_node;
 }
 
+int check_if_goalState(int the_arr[16], int the_goal_state[16]) {
+
+    int i = 0;
+    int total_count = 0;
+    int is_the_GOALSTATE = 0;
+
+    for(i = 0; i < 16; i++) {
+        if(the_arr[i] == the_goal_state[i])
+            total_count++;  // Max value is 16.
+        else
+            break;          // Means that the_arr[i] is not the goal state.
+    }
+    
+    if(total_count == 16)
+        is_the_GOALSTATE = 1;   // 1 = true, means that each value per index satisfies the goal state.
+    
+    return is_the_GOALSTATE;    // 0 = false, 1 = true
+
+}
+
 void main() {
     board_guide();
 }
@@ -537,5 +570,8 @@ void main() {
                 - This means that there will be no more children for the current level being expanded, and hence set the value of 0.
     [DONE] 2. Create an algorithm for checking whether a certain node cannot anymore be expanded.
             - This is where the SPRINT '1' is used.
-    3. Create an algorithm to check whether the node to be expanded is already the GOAL state.
+    [DONE] 3. Create an algorithm to check whether the node to be expanded is already the GOAL state.
+    
+    4. Create an algorithm/logic on what to do when expanded node is the GOAL state.
+    5. Create an algorithm to store the solution path.
 */
